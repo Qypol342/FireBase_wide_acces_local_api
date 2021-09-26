8a35c7f5fc690977475d077cdc4fea3eef9d059b
Maeght Loan <37497007+Qypol342@users.noreply.github.com>
2021-09-26 18:43:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pyrebase import pyrebase
 from Key import keys
+import time
 
 from ActuSpace import actuspace
 from ActuAviation import actuaviation
@@ -12,11 +13,18 @@ api_list =[actuspace, actuaviation]
 firebase = pyrebase.initialize_app(key)
 db = firebase.database()
 
-for i in range(len(api_list)):
-	data = api_list[i]()
-	db.child("NewsAPI").child(api_list[i].__name__).set(data)
+while True:
+	for i in range(len(api_list)):
+		data = api_list[i]()
+		db.child("NewsAPI").child(api_list[i].__name__).set(data)
+
+	time.sleep(60*10)
 
 
 
+
+
+"""
 res = db.child("NewsAPI").get()
 print(res.val()['actuspace'])
+"""
